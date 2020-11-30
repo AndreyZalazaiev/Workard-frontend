1e29e1e156bf9bd1fac6f78ae29b71d88878ce3d
Bergmah <52858177+Bergmah@users.noreply.github.com>
2020-11-30 18:29:43 +0200

    backup v2

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -7,7 +7,7 @@ import {Routes, RouterModule, Router} from '@angular/router';
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent implements OnInit {
 
